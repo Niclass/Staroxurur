9d385c5c12b5f1f44ac7524b8ad4cdb75d988dc6
Niclass <niclas.soder@gmail.com>
2015-11-26 11:44:38 +0100

    projektet

---
+++ StarFox/Source/StarFox/StarFoxGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "StarFoxGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STARFOX_API AStarFoxGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
